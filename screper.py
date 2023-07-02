506af38cca0003df0f206572b07c499c509d0b2f
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-07-02 23:56:21 +0100

    tabled movies name

--- screper.py
+++ screper.py
@@ -2,3 +2,4 @@ import pandas as pd
 url = 'https://mobiletvshows.net/subfolder-Arrow.htm'
 data_frame = pd.read_html('https://en.wikipedia.org/wiki/List_of_Arrow_episodes')
 print(data_frame[2])
+''
